5a1fd36d8c7989a2cc13d4c98a8465f73292163b
Andrii Revutskyi <andrew31101997@gmail.com>
2025-03-31 16:44:05 +0200

    Added button handling and improved game logic

--- js/script.js
+++ js/script.js
@@ -1,37 +1,53 @@
+var buttonRock = document.getElementById('button-rock');
+var buttonPaper = document.getElementById('button-paper');
+var buttonScissors = document.getElementById('button-scissors');
+
 function getMoveName(moveId) {
-  if (moveId == 1) {
-    return 'kamień';
-  } else if (moveId == 2) {
-    return 'papier';
-  } else if (moveId == 3) {
-    return 'nożyce';
-  } else {
-    return 'nieznany ruch';
-  }
+    if (moveId == 1) {
+        return 'kamień';
+    } else if (moveId == 2) {
+        return 'papier';
+    } else if (moveId == 3) {
+        return 'nożyce';
+    } else {
+        return 'nieznany ruch';
+    }
 }
 
 function displayResult(playerMove, computerMove) {
-  if (
-    (playerMove === 'kamień' && computerMove === 'nożyce') ||
-    (playerMove === 'papier' && computerMove === 'kamień') ||
-    (playerMove === 'nożyce' && computerMove === 'papier')
-  ) {
-    printMessage('Wygrywasz!');
-  } else if (playerMove === computerMove) {
-    printMessage('Remis!');
-  } else {
-    printMessage('Przegrywasz!');
-  }
+    if (
+        (playerMove === 'kamień' && computerMove === 'nożyce') ||
+        (playerMove === 'papier' && computerMove === 'kamień') ||
+        (playerMove === 'nożyce' && computerMove === 'papier')
+    ) {
+        printMessage('Wygrywasz!');
+    } else if (playerMove === computerMove) {
+        printMessage('Remis!');
+    } else {
+        printMessage('Przegrywasz!');
+    }
 }
 
-// Losowanie ruchu komputera
-var randomNumber = Math.floor(Math.random() * 3 + 1);
-var computerMove = getMoveName(randomNumber);
-printMessage('Mój ruch: ' + computerMove);
+function buttonClicked(argButtonName) {
+    clearMessages();
+    console.log(argButtonName + ' został kliknięty');
 
-// Wybór ruchu gracza
-var playerInput = prompt('Wybierz swój ruch: 1 - kamień, 2 - papier, 3 - nożyce');
-var playerMove = getMoveName(playerInput);
-printMessage('Twój ruch: ' + playerMove);
+    var randomNumber = Math.floor(Math.random() * 3 + 1);
+    var computerMove = getMoveName(randomNumber);
+    printMessage('Mój ruch: ' + computerMove);
 
-displayResult(playerMove, computerMove);
+    var playerMove = argButtonName;
+    printMessage('Twój ruch: ' + playerMove);
+
+    displayResult(playerMove, computerMove);
+}
+
+buttonRock.addEventListener('click', function() {
+    buttonClicked('kamień');
+});
+buttonPaper.addEventListener('click', function() {
+    buttonClicked('papier');
+});
+buttonScissors.addEventListener('click', function() {
+    buttonClicked('nożyce');
+});
