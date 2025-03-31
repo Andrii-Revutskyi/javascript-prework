527ff78422ce128d50b37a171e645c71f936dd25
Andrii Revutskyi <andrew31101997@gmail.com>
2025-03-31 16:14:40 +0200

    Completion of the getMoveName and displayResult functions

--- js/script.js
+++ js/script.js
@@ -1,33 +1,37 @@
-// Computer's move randomization
-var computerMove, randomNumber;
-randomNumber = Math.floor(Math.random() * 3 + 1);
-console.log('Wylosowana liczba to: ' + randomNumber);
-
-if (randomNumber == 1) {
-  computerMove = 'kamień';
-} else if (randomNumber == 2) {
-  computerMove = 'papier';
-} else if (randomNumber == 3) {
-  computerMove = 'nożyce';
-} else {
-  computerMove = 'nieznany ruch';
+function getMoveName(moveId) {
+  if (moveId == 1) {
+    return 'kamień';
+  } else if (moveId == 2) {
+    return 'papier';
+  } else if (moveId == 3) {
+    return 'nożyce';
+  } else {
+    return 'nieznany ruch';
+  }
 }
 
+function displayResult(playerMove, computerMove) {
+  if (
+    (playerMove === 'kamień' && computerMove === 'nożyce') ||
+    (playerMove === 'papier' && computerMove === 'kamień') ||
+    (playerMove === 'nożyce' && computerMove === 'papier')
+  ) {
+    printMessage('Wygrywasz!');
+  } else if (playerMove === computerMove) {
+    printMessage('Remis!');
+  } else {
+    printMessage('Przegrywasz!');
+  }
+}
+
+// Losowanie ruchu komputera
+var randomNumber = Math.floor(Math.random() * 3 + 1);
+var computerMove = getMoveName(randomNumber);
 printMessage('Mój ruch: ' + computerMove);
 
-// Player's move selection
+// Wybór ruchu gracza
 var playerInput = prompt('Wybierz swój ruch: 1 - kamień, 2 - papier, 3 - nożyce');
-var playerMove;
-
-if (playerInput == 1) {
-  playerMove = 'kamień';
-} else if (playerInput == 2) {
-  playerMove = 'papier';
-} else if (playerInput == 3) {
-  playerMove = 'nożyce';
-} else {
-  printMessage('Błędny wybór! Zakładamy, że wybrałeś kamień.');
-  playerMove = 'kamień';
-}
-
+var playerMove = getMoveName(playerInput);
 printMessage('Twój ruch: ' + playerMove);
+
+displayResult(playerMove, computerMove);
